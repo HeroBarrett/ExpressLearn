c5dcbe9e91895ec7a6078c2ddba38cd257c66498
Dixon <510672083@qq.com>
2025-09-15 09:18:39 +0800

    feat:封装响应，优化代码

--- README.md
+++ README.md
@@ -6,3 +6,19 @@
 运行迁移命令 npx sequelize-cli db:migrate 创建数据库表结构
 - d.
 可选：运行种子命令 npx sequelize-cli db:seed:all 填充测试数据
+
+
+
+# Sequelize 使用
+
+| 命令                                                         | 说明                                      |
+| ------------------------------------------------------------ | ----------------------------------------- |
+| sequelize db:create --charset utf8mb4 --collate utf8mb4_general_ci | 创建数据库-windows会有问题，到navicat创建 |
+| sequelize model:generate --name Article --attributes title:string,content:text | 创建模型(表名字单数)-到迁移文件微调       |
+| sequelize db:migrate                                         | 运行迁移命令                              |
+| sequelize seed:generate --name article                       | 创建种子文件                              |
+| sequelize db:seed --seed xxx-article                         | 运行指定种子文件                          |
+| sequelize db:seed:all                                        | 慎用，会把种子文件全部执行                |
+
+
+
--- routes/admin/articles.js
+++ routes/admin/articles.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const { Article } = require("../../models");
+const { Op } = require("sequelize");
+const { NotFoundError, success, failure } = require("../../utils/response");
 
 /**
  * 获取所有文章列表
@@ -8,26 +10,48 @@ const { Article } = require("../../models");
  */
 router.get("/", async function (req, res, next) {
   try {
-    // 定义倒序排列
+    // 拿到模糊查询的数据
+    const query = req.query;
+    // 获取分页所需的参数
+    // 当前第几页，默认第一页
+    const currentPage = Math.abs(Number(query.currentPage)) || 1;
+    // 每页显示多少条，默认10条
+    const pageSize = Math.abs(Number(query.pageSize)) || 10;
+    // 计算offset -> 在数据库中从第几条开始
+    const offset = (currentPage - 1) * pageSize;
+
+    // 配置项
     const condition = {
+      // 排序 -》 按照id倒序
       order: [["id", "DESC"]],
+      // 分页 -》 从第几条开始，显示多少条
+      offset,
+      // 每页显示多少条
+      limit: pageSize,
     };
+
+    // 模糊查询
+    if (query.title) {
+      condition.where = {
+        title: {
+          [Op.like]: `%${query.title}%`,
+        },
+      };
+    }
+
     // 从数据库获取数据
-    const articles = await Article.findAll(condition);
+    const { count, rows } = await Article.findAndCountAll(condition);
     // res.json({ message: "这里是后台的文章列表接口~" });
-    res.json({
-      status: true,
-      message: "获取文章列表成功",
-      data: {
-        articles,
+    success(res, "查询文章列表成功", {
+      articles: rows,
+      pagination: {
+        total: count, // 总条数
+        currentPage, // 当前第几页
+        pageSize, // 每页显示多少条
       },
     });
   } catch (error) {
-    res.status(500).json({
-      status: false,
-      message: "获取文章列表失败",
-      error: [error.message],
-    });
+    failure(res, error);
   }
 });
 
@@ -37,27 +61,97 @@ router.get("/", async function (req, res, next) {
  */
 router.get("/:id", async function (req, res) {
   try {
-    // 获取文章id
-    const{ id } = req.params;
-    // 查询文章
-    const article = await Article.findByPk(id);
-    console.log(article);
-    
+    const article = await getArticle(req);
+
     // 响应数据
+    success(res, "查询文章详情成功", {
+      article,
+    });
+  } catch (error) {
+    failure(res, error);
+  }
+});
+
+/**
+ * 创建文章
+ * POST /admin/articles
+ */
+router.post("/", async function (req, res, next) {
+  try {
+    // 白名单过滤
+    const body = filterBody(req);
+    // 插入数据库
+    const article = await Article.create(body);
+    // 响应数据
+    success(res, "创建文章成功", { article }, 201);
+  } catch (error) {
+    failure(res, error);
+  }
+});
+
+/**
+ * 删除文章
+ * Delete /admin/articles/:id
+ */
+router.delete("/:id", async function (req, res, next) {
+  try {
+    const article = await getArticle(req);
+    // 删除文章
+    await article.destroy();
+    // 响应数据
+    success(res, "删除文章成功");
+  } catch (error) {
+    failure(res, error);
+  }
+});
+
+/**
+ * 更新文章
+ * PUT /admin/articles/:id
+ */
+router.put("/:id", async function (req, res, next) {
+  try {
+    const article = await getArticle(req);
+    // 白名单过滤
+    const body = filterBody(req);
+    await article.update(body);
+    // 相应数据
     res.json({
       status: true,
-      message: "获取文章详情成功",
+      message: "更新文章成功",
       data: {
         article,
       },
-    })
+    });
   } catch (error) {
-    res.status(500).json({
-      status: false,
-      message: "获取文章详情失败",
-      error: [error.message],
-    })
+    failure(res, error);
   }
 });
+
+/**
+ * 公共方法：查询当前文章
+ */
+async function getArticle(req) {
+  // 获取文章id
+  const { id } = req.params;
+  // 查询文章
+  const article = await Article.findByPk(id);
+  if (!article) {
+    throw new NotFoundError("文章不存在");
+  }
+  return article;
+}
+
+/**
+ * 公共方法：白名单过滤
+ * @param erq
+ * @returns {{}}
+ */
+function filterBody(req) {
+  return {
+    title: req.body.title,
+    content: req.body.content,
+  };
+}
 
 module.exports = router;
